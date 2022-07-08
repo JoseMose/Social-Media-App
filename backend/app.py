5abd7e04bc8b3ca6977662a906d8ff816c3f9abe
Joseph <josephesfandiari@gmail.com>
2022-07-07 23:14:59 -0400

    built the framework

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,26 @@
-from flask import Flask, appcontext_popped
+from urllib import response
+from flask import Flask, render_template
+import requests
+from sqlalchemy import true
+import json
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
 
 app = Flask(__name__)
 
 @app.route('/main')
-def main_page():
-    Hello = "Hello World!"
-    return Hello
+def main():
+    api = requests.get("https://data.nasdaq.com/api/v3/datatables/ZILLOW/DATA?indicator_id=ZSFH&region_id=102001&api_key=AxG8LscgzpqxLLHjrfa4")
+    response = api.json()
+    responding=response
+    return {'api': responding}
 
+@app.route('/time')
+def hello():
+    api = requests.get("http://worldtimeapi.org/api/timezone/America/New_York")
+    response = api.json()
+    responding=response
+    return {'time': responding}
 
-app.run()
+app.run(debug=true)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,33 @@
-import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import "./App.css"
 
 function App() {
-  const[getMessage, setGetMessage] = useState(({}))
-    useEffect(()=>{
-    axios.get('/main').then(response => {
-        setGetMessage(response)
-    })
-    }, [])
+  const[getHousingMarket, setGetHousingMarket] = useState(0)
+  const[getTime, setGetTime] = useState(0)
+  useEffect(() => {
+    fetch('/main').then(res => res.json()).then(data => {
+      setGetHousingMarket(data.api.datatable.data);
+    });
+  }, []);
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setGetTime(data.time.datetime);
+    });
+  }, []);
   return (
+    <>
+    <div className='header'>
+      <h1 className='header1'>Today's Breaking News</h1>
+      <h1 className='header2'>My Account</h1>
+      <h1>Coming Soon...</h1>
+      <h1 className='header3'>Log Out</h1>
+    </div>
       <div>
-        <h3>{getMessage.data}</h3>
+        <div>{getTime}</div>
+        <div>{getHousingMarket}</div>
       </div>
+    </>
   );
 }
 export default App;
